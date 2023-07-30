0f8c8f2547394c6bfbe7032d2b60d8dbd8f78da5
Pu Fanyi <pufanyi@gmail.com>
2023-07-30 11:45:26 +0800

    fix the bug when testing

---
+++ tests/prompts/test_in_context_example.py
@@ -0,0 +1,1 @@
+from syphus import InContextExample, QAPair
--- tests/prompts/test_qa_pair.py
+++ tests/prompts/test_qa_pair.py
@@ -3,7 +3,7 @@ import yaml
 from syphus import QAPair
 
 
-with open("../data/dense_captions_prompt.yaml", "r") as f:
+with open("tests/data/dense_captions_prompt.yaml", "r") as f:
     sample_qa_pair = yaml.safe_load(f)["in_context_examples"][0]["assistant"][0]
 
 
