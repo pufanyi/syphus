5e1d67e131628d2eb677bc6d8c9deb46e5001758
Pu Fanyi <pufanyi@gmail.com>
2023-07-31 17:41:03 +0800

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,3 +2,6 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+
+if not os.path.exists("tests/test_output"):
+    os.makedirs("tests/test_output")
