2190b78b7f72e5783d37b192b1330ca46b9b1922
Pu Fanyi <pufanyi@gmail.com>
2023-08-06 21:36:09 +0800

    from gpt_info_path import gpt info

--- src/syphus/data_generator/gpt_manager.py
+++ src/syphus/data_generator/gpt_manager.py
@@ -1,9 +1,10 @@
 import openai
 import time
+import sys
 
 from syphus import prompts
-from syphus.data_generator.gpt_params_settings import GPTParamsSettings
-from syphus.data_generator.openai_settings import OpenAISettings
+import syphus.data_generator.gpt_params_settings as gpt_params_settings
+import syphus.data_generator.openai_settings as openai_settings
 
 from typing import Optional
 
@@ -11,27 +12,39 @@ from typing import Optional
 class GPTManager(object):
     def __init__(
         self,
-        openai_api: OpenAISettings,
         *,
-        gpt_params: Optional[GPTParamsSettings] = None,
+        gpt_info_path: Optional[str] = None,
+        openai_api: Optional[openai_settings.OpenAISettings] = None,
+        gpt_params: Optional[gpt_params_settings.GPTParamsSettings] = None,
     ):
-        self.openai_api = openai_api
-        if gpt_params:
-            self.gpt_params = gpt_params
+        if gpt_info_path:
+            if openai_api or gpt_params:
+                print(
+                    "Warning: gpt_info_path overrides openai_api and gpt_params",
+                    flush=True,
+                    file=sys.stderr,
+                )
+            self.openai_api = openai_settings.read_yaml(gpt_info_path)
+            self.gpt_params = gpt_params_settings.read_yaml(gpt_info_path)
+        elif openai_api:
+            self.openai_api = openai_api
+            if gpt_params:
+                self.gpt_params = gpt_params
+            else:
+                self.gpt_params = gpt_params_settings.GPTParamsSettings()
         else:
-            self.gpt_params = GPTParamsSettings()
+            raise ValueError("Must provide either gpt_info_path or openai_api")
 
-    def set_gpt_params(self, gpt_params: GPTParamsSettings):
+    def set_gpt_params(self, gpt_params: gpt_params_settings.GPTParamsSettings):
         self.gpt_params = gpt_params
 
     def query_gpt(self, prompt: prompts.Prompts):
-        messages = prompt.get_messages()
         succuss = False
         while not succuss:
             try:
                 response = openai.ChatCompletion.create(
                     engine=self.openai_api.engine,
-                    messages=messages,
+                    messages=prompt.get_messages(),
                     temperature=self.gpt_params.temperature,
                     max_tokens=self.max_tokens,
                     top_p=self.gpt_params.top_p,
