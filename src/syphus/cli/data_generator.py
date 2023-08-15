2e483ca37322c6378f014b1a572a4fdf5035f63b
Pu Fanyi <pufanyi@gmail.com>
2023-08-15 12:34:09 +0800

    add command for query

--- src/syphus/cli/data_generator.py
+++ src/syphus/cli/data_generator.py
@@ -1,7 +1,21 @@
 def query_command(subparsers):
     query_parser = subparsers.add_parser("query", help="Query GPT for QA pairs.")
-    query_parser.add_argument("-s", "--source", help="Source file")
-    query_parser.add_argument("-m", "--mode", help="Mode")
+    query_parser.add_argument(
+        "-c", "--config", help="OpenAI Config File", required=True
+    )
+    query_parser.add_argument(
+        "-i", "--input", help="Input File of Information", required=True
+    )
+    query_parser.add_argument(
+        "-s", "--split", action="store_true", help="Split every information"
+    )
+    query_parser.add_argument("-p", "--prompts", help="Prompts File", required=True)
+    query_parser.add_argument(
+        "--prompts_format",
+        help="Format of prompts file",
+        default="auto",
+        choices=["auto", "json", "yaml", "yml"],
+    )
     query_parser.set_defaults(func=query)
 
 
