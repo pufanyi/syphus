3a22ba4b093006538d5ccc3a55bf9ae84b3f2b65
Pu Fanyi <pufanyi@gmail.com>
2023-08-17 14:06:49 +0800

    init

--- src/syphus/cli/initializer.py
+++ src/syphus/cli/initializer.py
@@ -1,23 +1,22 @@
 import os
+import shutil
 import pkg_resources
 
 
 def init_command(subparsers):
     init_parser = subparsers.add_parser("init", help="Initialize the project.")
     init_parser.add_argument("folder", help="Folder to initialize the project in")
-    # init_parser.add_argument("-s", "--source", help="Source file")
-    # init_parser.add_argument("-m", "--mode", help="Mode")
     init_parser.set_defaults(func=init)
 
 
 def init(args):
-    if not os.path.exists(args.folder):
-        os.makedirs(args.folder)
-    elif os.listdir(args.folder):
-        raise FileExistsError("Folder is not empty")
+    if os.path.exists(args.folder):
+        if os.listdir(args.folder):
+            raise FileExistsError("Folder is not empty")
+        else:
+            os.rmdir(args.folder)
 
     resource_path = pkg_resources.resource_filename("syphus", "resources")
-    assert os.path.exists(
-        resource_path
-    ), f"Template folder does not exist: {resource_path}"
-    # raise ValueError(resource_path)
+    template_path = os.path.join(resource_path, "template")
+
+    shutil.copytree(template_path, args.folder)
--- tests/cli/test_init.py
+++ tests/cli/test_init.py
@@ -1,6 +1,38 @@
+import os
+import filecmp
 import subprocess
 
 
+def same_file(file1, file2):
+    assert os.path.isfile(file1) and os.path.isfile(
+        file2
+    ), f"One of {file1} and {file2} is not a file"
+    with open(file1, "r") as f1, open(file2, "r") as f2:
+        assert f1.read() == f2.read(), f"{file1} and {file2} are not the same"
+
+
+def same_folder(folder1, folder2):
+    if os.path.isfile(folder1) or os.path.isfile(folder2):
+        same_file(folder1, folder2)
+        return
+
+    assert os.path.isdir(folder1) and os.path.isdir(
+        folder2
+    ), f"One of {folder1} and {folder2} is not both a folder or a file"
+
+    comparison = filecmp.dircmp(folder1, folder2)
+    assert (
+        not comparison.left_only
+        and not comparison.right_only
+        and not comparison.diff_files
+    ), f"{folder1} and {folder2} are not the same"
+
+    for common_dir in comparison.common_dirs:
+        same_folder(
+            os.path.join(folder1, common_dir), os.path.join(folder2, common_dir)
+        )
+
+
 def test_init():
-    subprocess.run(["syphus", "init", "tests/test_output"])
-    assert True
+    subprocess.run(["syphus", "init", "tests/test_output/init"])
+    same_folder("tests/test_output/init", "src/syphus/resources/template")
