7ed56916e5e9c7d979daeb2cfb7b1042baa96e5c
Pu Fanyi <pufanyi@gmail.com>
2023-08-22 00:16:38 +0800

    lint

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,22 +6,21 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Syphus'
-copyright = '2023, Fanyi Pu'
-author = 'Fanyi Pu'
+project = "Syphus"
+copyright = "2023, Fanyi Pu"
+author = "Fanyi Pu"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
