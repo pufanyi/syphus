3995246c5ec802667b0fdde37d290141a2871f82
Pu Fanyi <pufanyi@gmail.com>
2023-08-21 17:47:07 +0800

    fix a small bug

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="syphus",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
