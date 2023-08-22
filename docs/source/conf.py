47b68dd6ffafc749b8e38c381f48fc1fc3c6bafc
Pu Fanyi <pufanyi@gmail.com>
2023-08-22 11:14:22 +0800

    change theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,5 +22,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+html_theme = "python_docs_theme"
 html_static_path = ["_static"]
