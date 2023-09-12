f57d435dd1831255fac3eb5742e573e16e0fe447
Pu Fanyi <pufanyi@gmail.com>
2023-09-12 12:38:31 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="syphus",
-    version="0.0.5.0",
+    version="0.0.5.1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     install_requires=[
